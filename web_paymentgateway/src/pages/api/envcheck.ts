33b2396050f76a3398f5d129b733e5e1ac3bf00a
ulffiia <ullfiarahma2@gmail.com>
2025-09-26 22:40:28 +0700

    bismillah bisaaa

--- web_paymentgateway/src/pages/api/envcheck.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({
-    hasMongoUri: Boolean(process.env.MONGODB_URI),
-    uriPrefix: process.env.MONGODB_URI?.slice(0, 20), // aman: hanya prefix
-  });
-}
