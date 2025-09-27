e75a807998967a163b946efb4f1032cfe85d2f2e
ulffiia <ullfiarahma2@gmail.com>
2025-09-27 15:46:35 +0700

    perbaikan code payment

--- web_paymentgateway/src/app/payment-success/page.tsx
+++ web_paymentgateway/src/app/payment-success/page.tsx
@@ -1,17 +1,19 @@
+import Link from "next/link";
+
 export default function PaymentSuccess() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-green-50">
       <div className="bg-white shadow-lg rounded-lg p-8 text-center">
-        <h1 className="text-2xl font-bold text-green-600">✅ Payment Successful!</h1>
+        <h1 className="text-2xl font-bold text-green-600">Payment Successful!</h1>
         <p className="mt-4 text-gray-700">
-          Terima kasih, pembayaran Anda berhasil diproses.  
+          Terima kasih, pembayaran Anda berhasil diproses.
         </p>
-        <a
+        <Link
           href="/"
           className="mt-6 inline-block bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700"
         >
           Kembali ke Home
-        </a>
+        </Link>
       </div>
     </div>
   );
